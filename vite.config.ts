2af906508ef38e85aebb4cd0d2bb01b1531354ed
Sergey Kononov <sergey.kononov@vk.team>
2025-04-16 18:05:08 +0300

    add react test lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig, } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+    plugins: [react(), tailwindcss()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: '/vitest-setup.ts',
+        include: ['**/*.test.{ts,tsx}']
+    },
+} as UserConfig)
