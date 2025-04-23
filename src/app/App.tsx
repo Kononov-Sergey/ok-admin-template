b7d5f6f7a652669caf757b36b8f5dc8e15872d67
Sergey Kononov <sergey.kononov@vk.team>
2025-04-23 11:04:43 +0300

    убрать zustand из шаблона

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,7 @@
-import { useCounter } from "./model/counter";
-
 const App = () => {
-  const { inc, count } = useCounter();
   return (
     <main className="flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold text-center text-blue-500">Tailwind is working!</h1>
-      <div className="text-2xl font-bold">{count}</div>
-      <button className="bg-blue-500 text-white p-2 rounded-md cursor-pointer" onClick={inc}>
-        Zustand is working! Click me!
-      </button>
     </main>
   );
 };
